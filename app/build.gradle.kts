7bed1f67b4db0d94bbcd6ff3120a1816c8a559fb
umer0586 <umerfarooq.phone@gmail.com>
2025-02-27 16:05:45 +0500

    Version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.umer0586.droidpad"
         minSdk = 21
         targetSdk = 35
-        versionCode = 16
-        versionName = "2.0.0"
+        versionCode = 17
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
