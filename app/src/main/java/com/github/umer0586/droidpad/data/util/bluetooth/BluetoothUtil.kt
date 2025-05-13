2594e16baeed826d8afb2bfb691fa2c50fd3e735
umer0586 <umerfarooq.phone@gmail.com>
2025-05-13 16:25:17 +0500

    Added broadcast receiver to observe bluetooth state

--- app/src/main/java/com/github/umer0586/droidpad/data/util/bluetooth/BluetoothUtil.kt
+++ app/src/main/java/com/github/umer0586/droidpad/data/util/bluetooth/BluetoothUtil.kt
@@ -1,9 +1,17 @@
 package com.github.umer0586.droidpad.data.util.bluetooth
 
 import com.github.umer0586.droidpad.data.connectionconfig.RemoteBluetoothDevice
+import kotlinx.coroutines.flow.Flow
+
+data class BluetoothState(
+    val isEnable: Boolean,
+    val pairedDevices: List<RemoteBluetoothDevice>
+)
 
 interface BluetoothUtil {
+    val bluetoothState: Flow<BluetoothState>
     fun isBluetoothEnabled() : Boolean
     fun getPairedDevices() : List<RemoteBluetoothDevice>
     fun hasBluetoothPermission() : Boolean
+    fun cleanUp(): Unit
 }
--- app/src/main/java/com/github/umer0586/droidpad/data/util/bluetooth/BluetoothUtilImp.kt
+++ app/src/main/java/com/github/umer0586/droidpad/data/util/bluetooth/BluetoothUtilImp.kt
@@ -1,17 +1,70 @@
 package com.github.umer0586.droidpad.data.util.bluetooth
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
+import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.github.umer0586.droidpad.data.connectionconfig.RemoteBluetoothDevice
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class BluetoothUtilImp(private val applicationContext: Context): BluetoothUtil {
 
+
     private val bluetoothManager =
         applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+
+    private val broadcastMessageReceiver = BroadcastMessageReceiver(applicationContext)
+
+    private val _bluetoothState = MutableStateFlow(
+        BluetoothState(
+            isEnable = isBluetoothEnabled(),
+            pairedDevices = getPairedDevices()
+        )
+    )
+
+    override val bluetoothState: Flow<BluetoothState>
+        get() = _bluetoothState.asStateFlow()
+
+
+    init {
+
+
+        broadcastMessageReceiver.registerEvents(IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED))
+
+        broadcastMessageReceiver.setOnMessageReceived { intent ->
+            if (BluetoothAdapter.ACTION_STATE_CHANGED == intent.action) {
+                val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
+                when (state) {
+                    BluetoothAdapter.STATE_OFF -> {
+                        Log.d("Bluetooth", "Bluetooth is OFF")
+                        _bluetoothState.value = BluetoothState(
+                            isEnable = false,
+                            pairedDevices = getPairedDevices()
+                        )
+                    }
+                    BluetoothAdapter.STATE_ON -> {
+                        Log.d("Bluetooth", "Bluetooth is ON")
+                        _bluetoothState.value = BluetoothState(
+                            isEnable = true,
+                            pairedDevices = getPairedDevices()
+                        )
+                    }
+
+                }
+            }
+        }
+    }
 
     override fun isBluetoothEnabled() = bluetoothManager.adapter.isEnabled
 
@@ -58,5 +111,73 @@ class BluetoothUtilImp(private val applicationContext: Context): BluetoothUtil {
         }
 
     }
+
+    override fun cleanUp() {
+        broadcastMessageReceiver.unregisterEvents()
+    }
+
+}
+
+private class BroadcastMessageReceiver(private val context: Context) : BroadcastReceiver()
+{
+    private var messageReceiveCallBack : ((Intent) -> Unit)? = null
+    private var isRegistered = false
+
+    companion object
+    {
+        private val TAG: String = BroadcastMessageReceiver::class.java.simpleName
+    }
+
+    fun setOnMessageReceived(callBack : ((Intent) -> Unit)?)
+    {
+        messageReceiveCallBack = callBack
+    }
+
+    override fun onReceive(context: Context, intent: Intent?)
+    {
+        Log.d(TAG, "onReceive() intent = [$intent]")
+        if (intent != null) {
+            messageReceiveCallBack?.invoke(intent)
+        }
+
+    }
+
+    fun registerEvents(intentFilter: IntentFilter)
+    {
+        Log.d(TAG, "registerEvents() called")
+
+        try
+        {
+            if (!isRegistered)
+                ContextCompat.registerReceiver(context,this, intentFilter,ContextCompat.RECEIVER_NOT_EXPORTED)
+
+
+            isRegistered = true
+        }
+        catch (e: IllegalArgumentException)
+        {
+            isRegistered = false
+            e.printStackTrace()
+        }
+    }
+
+    fun unregisterEvents()
+    {
+        Log.d(TAG, "unregister() called")
+
+        try
+        {
+            if (isRegistered)
+                context.unregisterReceiver(this)
+
+            isRegistered = false
+        }
+        catch (e: IllegalArgumentException)
+        {
+            isRegistered = false
+            e.printStackTrace()
+        }
+    }
+
 
 }
