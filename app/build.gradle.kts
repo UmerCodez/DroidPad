a3d1479fbd796c7f7851d87a38f478cdf6a6b9da
umer0586 <umerfarooq.phone@gmail.com>
2025-06-03 05:49:15 +0500

    build: update version code to 26 and version name to 2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.github.umer0586.droidpad"
         minSdk = 21
         targetSdk = 35
-        versionCode = 25
-        versionName = "2.8.0"
+        versionCode = 26
+        versionName = "2.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
