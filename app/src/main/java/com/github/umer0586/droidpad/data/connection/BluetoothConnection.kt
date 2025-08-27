57e881fc7c06851af984412855f528b0338a78bb
umer0586 <umerfarooq2383@gmail.com>
2025-08-27 16:24:17 +0500

    Remove unused import `java.io.InputStreamReader` in `BluetoothConnection.kt`

--- app/src/main/java/com/github/umer0586/droidpad/data/connection/BluetoothConnection.kt
+++ app/src/main/java/com/github/umer0586/droidpad/data/connection/BluetoothConnection.kt
@@ -34,11 +34,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.OutputStream
 import java.io.BufferedReader
+import java.io.OutputStream
 import java.util.UUID
 
 class BluetoothConnection(
