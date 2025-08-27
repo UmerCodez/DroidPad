82800d2d58688d93ce7b0ec5250335cd9fe0c5f5
umer0586 <umerfarooq2383@gmail.com>
2025-08-27 16:23:47 +0500

    Remove unused import `RectangleShape`
    This commit removes an unused import of `RectangleShape` from the `ControlPadButton.kt` file.

--- app/src/main/java/com/github/umer0586/droidpad/ui/components/ControlPadButton.kt
+++ app/src/main/java/com/github/umer0586/droidpad/ui/components/ControlPadButton.kt
@@ -46,7 +46,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
