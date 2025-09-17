49cf8e1393bd8313f962954d65063027f257dd4e
umer0586 <umerfarooq.phone@gmail.com>
2025-09-17 16:26:04 +0500

    build: update version code to 30 and version name to 3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.github.umer0586.droidpad"
         minSdk = 21
         targetSdk = 35
-        versionCode = 29
-        versionName = "3.1.0"
+        versionCode = 30
+        versionName = "3.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
