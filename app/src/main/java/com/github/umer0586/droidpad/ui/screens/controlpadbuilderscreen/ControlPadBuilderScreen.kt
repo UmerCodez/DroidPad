cd129e5ac7eb6090a874457b882e13a042f4e67e
UmerCodez <umerfarooq.phone@gmail.com>
2025-11-29 20:42:23 +0500

    feat: update build icon to settings icon

--- app/src/main/java/com/github/umer0586/droidpad/ui/screens/controlpadbuilderscreen/ControlPadBuilderScreen.kt
+++ app/src/main/java/com/github/umer0586/droidpad/ui/screens/controlpadbuilderscreen/ControlPadBuilderScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -269,7 +270,7 @@ fun ControlPadBuilderScreenContent(
                         },
                         content = {
                             Icon(
-                                imageVector = Icons.Filled.Build,
+                                imageVector = Icons.Filled.Settings,
                                 contentDescription = "AidsIcon"
                             )
                         }
