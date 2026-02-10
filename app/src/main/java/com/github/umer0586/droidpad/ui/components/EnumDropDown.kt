224e83704ce6dccf0a04ae9b4fab551615aece5b
UmerCodez <umerfarooq.phone@gmail.com>
2026-02-10 16:01:27 +0500

    fix: Replace deprecated api

--- app/src/main/java/com/github/umer0586/droidpad/ui/components/EnumDropDown.kt
+++ app/src/main/java/com/github/umer0586/droidpad/ui/components/EnumDropDown.kt
@@ -27,7 +27,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -60,7 +60,7 @@ inline fun <reified T : Enum<T>> EnumDropdown(
         onExpandedChange = { dropdownExpanded = !dropdownExpanded }
     ) {
         OutlinedTextField(
-            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable, enabled = true),
+            modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable, enabled = true),
             label = { Text(label) },
             enabled = enabled,
             value = selectedValue.name,
